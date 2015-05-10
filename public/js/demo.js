6ec11b4435e2afa696f6dbfc7d8619f8ae9bb506
Michael Milewski <michael.milewski@redbubble.com>
2015-05-10 23:58:05 +1000

    load buttons and demo.js to run them and fetch code samples

---
+++ public/js/demo.js
@@ -0,0 +1,27 @@
+var Demo = (function(my){
+
+    function fetchJs(path, callback) {
+        var request = new XMLHttpRequest();
+        request.open("GET", path, true);
+        request.onreadystatechange = function() {
+            if (request.readyState != 4 || request.status != 200) return;
+            callback(request.responseText);
+        };
+        request.send();
+    }
+
+    function writeToSimulatorInput(elementId, text, runText) {
+        document.getElementById(elementId).value = text + runText;
+    }
+
+    my.fetchJs = function(elementId, jsPath, call) {
+        var runText = "\n\nSunspots.run(" + call + ")";
+        fetchJs("/js/" + jsPath, function(text) { writeToSimulatorInput(elementId, text, runText) })
+    };
+
+    my.eval = function(elementId) {
+        eval(document.getElementById(elementId).value);
+    };
+    return(my);
+
+}(Demo || {}));
